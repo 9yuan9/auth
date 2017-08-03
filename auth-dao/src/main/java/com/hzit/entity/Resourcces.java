0f94a0fc2e66a7a0ae65ff96af0e90d51a679b27
响 <Lee996118@foxmail.com>
2017-08-03 19:27:38 +0800

    李响Resourcces

---
+++ auth-dao/src/main/java/com/hzit/entity/Resourcces.java
@@ -0,0 +1,124 @@
+package com.hzit.entity;/**
+ * Created by 响 on 2017/8/3.
+ */
+
+/**
+ * 　　  　  　     \\\|///
+ * 　　　 　   　  \\ - -//
+ * 　　　　　   　(　 @.@　)
+ * +-------oOOo-----( v )-----oOOo--------------------------------------------+
+ * |　@author Lee　　　　　　                                                                 　|
+ * |　@author jxufe                                                   |
+ * |　@create 2017/8/3 19:10
+ * |  @resourcces
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class Resourcces {
+    private int rid;
+    private String rname;
+    private String resurl;
+    private int resorderno;
+    private String redsdes;
+
+    public int getRid() {
+        return rid;
+    }
+
+    public void setRid(int rid) {
+        this.rid = rid;
+    }
+
+    public String getRname() {
+        return rname;
+    }
+
+    public void setRname(String rname) {
+        this.rname = rname;
+    }
+
+    public String getResurl() {
+        return resurl;
+    }
+
+    public void setResurl(String resurl) {
+        this.resurl = resurl;
+    }
+
+    public int getResorderno() {
+        return resorderno;
+    }
+
+    public void setResorderno(int resorderno) {
+        this.resorderno = resorderno;
+    }
+
+    public String getRedsdes() {
+        return redsdes;
+    }
+
+    public void setRedsdes(String redsdes) {
+        this.redsdes = redsdes;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(String updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public String getUpdateMan() {
+        return updateMan;
+    }
+
+    public void setUpdateMan(String updateMan) {
+        this.updateMan = updateMan;
+    }
+
+    public String getCreateMan() {
+        return createMan;
+    }
+
+    public void setCreateMan(String createMan) {
+        this.createMan = createMan;
+    }
+
+    public String getParentID() {
+        return parentID;
+    }
+
+    public void setParentID(String parentID) {
+        this.parentID = parentID;
+    }
+
+    private String createTime;
+    private String updatetime;
+    private String createMan;
+    private String updateMan;
+    private String parentID;
+
+    @Override
+    public String toString() {
+        return "Resourcces{" +
+                "rid=" + rid +
+                ", rname='" + rname + '\'' +
+                ", resurl='" + resurl + '\'' +
+                ", resorderno=" + resorderno +
+                ", redsdes='" + redsdes + '\'' +
+                ", createTime='" + createTime + '\'' +
+                ", updatetime='" + updatetime + '\'' +
+                ", createMan='" + createMan + '\'' +
+                ", updateMan='" + updateMan + '\'' +
+                ", parentID='" + parentID + '\'' +
+                '}';
+    }
+}
