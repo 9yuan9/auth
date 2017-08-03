eb00554b24dde9efa5c4b03c3d1da4ac62f34111
9yuan9 <xieshuyuanzh@163.com>
2017-08-03 17:31:09 +0800

    提交用户角色关联表信息类和用户角色关联表接口类

---
+++ auth-dao/src/main/java/com/hzit/entity/UrInfo.java
@@ -0,0 +1,33 @@
+package com.hzit.entity;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 谢树源　　　　　                                                                 　|
+ * |　@author 深圳合众艾特信息技术有限公司                                                   |
+ * |　@create 2017年08月03日 - 17:25
+ * |    @description   用户角色关联表信息类
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class UrInfo {
+    private int uid;
+    private int rid;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getRid() {
+        return rid;
+    }
+
+    public void setRid(int rid) {
+        this.rid = rid;
+    }
+}
