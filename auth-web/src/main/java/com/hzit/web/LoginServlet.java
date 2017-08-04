adcef0802c7af8f8d1c74d6e9413c3fc2a95595b
9yuan9 <xieshuyuanzh@163.com>
2017-08-04 10:30:35 +0800

    导入网页

---
+++ auth-web/src/main/java/com/hzit/web/LoginServlet.java
@@ -0,0 +1,16 @@
+package com.hzit.web;
+
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class LoginServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
