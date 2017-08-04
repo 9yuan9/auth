a4e11e0920e42ec09f3fccb55248a1559de1f9cd
xiewenqiang <315128276@qq.com>
2017-08-04 14:34:18 +0800

    RoleServlet

---
+++ auth-web/src/main/java/com/hzit/servlet/RoleServlet.java
@@ -0,0 +1,32 @@
+package com.hzit.servlet;
+
+import com.hzit.dao.RoleinfoDao;
+import com.hzit.dao.SqlSessionHelper;
+import com.hzit.entity.Roleinfo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by acer on 2017/8/4.
+ */
+@WebServlet(name = "RoleServlet", value = "/addRole")
+public class RoleServlet extends HttpServlet {
+    RoleinfoDao roleinfoDao = SqlSessionHelper.getSqlSession().getMapper(RoleinfoDao.class);
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Roleinfo> allRole=roleinfoDao.findAll();
+        request.setAttribute("allRole",allRole);
+        request.getRequestDispatcher("addrole.jsp").forward(request, response);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
